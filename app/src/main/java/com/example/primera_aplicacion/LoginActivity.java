e65f891b1281a1c05fe47758f624208689c314da
Martin Antonio <mancorge@gmail.com>
2023-01-07 12:42:00 +0100

    Añadido checkbox para recordar el usuario

--- app/src/main/java/com/example/primera_aplicacion/LoginActivity.java
+++ app/src/main/java/com/example/primera_aplicacion/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.example.primera_aplicacion;
 
+import static com.example.primera_aplicacion.common.Constants.SP_PREFERENCES_DIRECTORY;
+import static com.example.primera_aplicacion.common.Constants.SP_USERS_KEY;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
@@ -13,6 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     // Variables de objetos en activity layout
     private Button btnEntrar; //Almacenará el botón ENTRAR
     private EditText etUser, etPassword; //Almacenará datos de campos usuario y contraseña
+    private CheckBox rememberUser; //Almacenará la información sobre si se recuerda o no el usuario
 
 
     @Override
@@ -24,9 +30,13 @@ public class LoginActivity extends AppCompatActivity {
         btnEntrar = findViewById(R.id.btnEntrar);
         etUser = findViewById(R.id.etUser);
         etPassword = findViewById(R.id.etPassword);
+        rememberUser = findViewById(R.id.checkBox);
 
         // Asignamos una acción de click al botón
         btnEntrar.setOnClickListener(doLogin);
+
+        // Si hay datos guardados en las sharedpreferences, los cargamos
+        loadUserName();
 
     }
 
@@ -38,6 +48,13 @@ public class LoginActivity extends AppCompatActivity {
             //Asignamos valor a variables locales para facilitar la lectura del código.
             String user = String.valueOf(etUser.getText());
             String password = etPassword.getText() + "";
+            boolean remember = rememberUser.isChecked();
+
+            //Primero si se ha marcado "recordar usuario" debemos almacenar el valor del campo usuario en un sharedpreference
+            //En tal caso la variable remember = true, si no se marca será false.
+            //Si remember = true, guardamos el valor de user con el método saveUserName(), caso contrario el valor se establece en null
+            //Usamos el operador ternario.
+            saveUserName(remember ? user : null);
 
             // Si el método areCredencialsValid devuelve true, se crea el Intent y se lanza la siguiente Activity
             if (areCredencialsValid(user, password)) {
@@ -74,4 +91,35 @@ public class LoginActivity extends AppCompatActivity {
         }
 
     };
+
+    //Método para guardar el valor del campo usuario en un sharedpreference
+    private void saveUserName(String user){
+        //Cargamos las sharedpreference guardadas
+            /*
+                Con MODE_PRIVATE como parámetro del método getSharedPreferences evitamos que otras apps puedan hacer uso
+                de lo que se guarde en sharedpreferences.
+                savedOptions es el nombre que recibe "el fichero" donde se almacenará la información.
+             */
+        SharedPreferences spSaved = LoginActivity.this.getSharedPreferences(SP_PREFERENCES_DIRECTORY,MODE_PRIVATE); //Obtenemos info almacenada, solo modo lectura
+
+        //Creamos un editor de los sharedpreferences
+        SharedPreferences.Editor spEditor = spSaved.edit();
+
+        //Para guardar el nombre de usuario usamos un putString, recibe clave -> Valor. Hay que terminar la transacción con un commit o apply
+        spEditor.putString(SP_USERS_KEY,user);
+        spEditor.apply(); //Si en lugar de apply usamos commit, hay que hacerlo en el hilo principal de la aplicación. Puede ralentizar la app si hay muchos datos.
+    }
+
+    //Método para recuperar el valor del campo usuario cuando se haya guardado en sharedpreferences
+    private void loadUserName(){
+        //Cargamos las sharedpreferences guardadas.
+        SharedPreferences spSaved = getSharedPreferences(SP_PREFERENCES_DIRECTORY, MODE_PRIVATE);
+
+        //Asignamos el valor de la clave USER al string, valor por defecto null (si no se ha guardado nada).
+        String user = spSaved.getString(SP_USERS_KEY,null);
+
+        //Seteamos si el checkbox debe ir marcado o no. La condición (user != null) dará falso si está vacío (unchecked) y true si tiene contenido (checked)
+        rememberUser.setChecked(user != null);
+        etUser.setText(user);
+    }
 }
---
+++ app/src/main/java/com/example/primera_aplicacion/common/Constants.java
@@ -0,0 +1,9 @@
+package com.example.primera_aplicacion.common;
+
+public class Constants {
+
+    // Constantes usadas a lo largo de la activity
+    public static final String SP_USERS_KEY = "USER"; //Esta se usará como KEY de las sharedPreferences
+    public static final String SP_PREFERENCES_DIRECTORY = "savedOptions"; //Esta se usará como ruta de almacenamiento de sharedPreferences
+
+}
